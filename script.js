67b59adac271c447a7614e5341fcdc2cf604e9fd
Amjad63043 <amjad63043@gmail.com>
2022-08-28 00:54:37 +0530

    helloiam

---
+++ script.js
@@ -0,0 +1,28 @@
+/*let js='amazing';
+if(js==="amazing")alert("javaScript is fun");
+ 
+40+80+90-20;
+console.log(40+80+90-2);
+let firstName="amjad";
+console.log(firstName);
+console.log(firstName); 
+let age =30;
+age =31;
+console.log(age);
+
+//const birthYear=29;
+//birthYear=31;
+//console.log(birthYear);
+
+
+//const job;
+
+var job='program';
+
+job='some'
+*/
+const now = 2037;
+const ageJohn =now-1991;
+const ageSarah=now-2018;
+console.log(ageJohn,ageSarah);
+console.log(ageJohn*2,ageJohn/10,2**3);
